707771d7b0fd219a6e090202fa142a952eb5c697
Max <mjann@ucsc.edu>
2019-06-07 12:35:44 -0700

    chess

--- src/App.js
+++ src/App.js
@@ -5,20 +5,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <div id="img">
+          <span id="chess">Chess Openings</span>
+        </div>
+        <div id="article">
+          <span id="jermaine">Jermaine beats me by countering the King's Indian Defense</span>
+        </div>
+      </div>
     </div>
   );
 }
